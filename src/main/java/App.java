d20079e531a1c9b905b52ff7db6d2bc328e3fe4b
nail-not-a-nail <nolik-yanaul@hotmail.com>
2026-06-09 10:52:13 +0500

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        System.out.println("ссылки хеллоуворлд одинаковы? Ответ: " + (bean==bean2));
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getCat());
+        Cat bean4 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean4.getCat());
+        System.out.println("ссылки котов одинаковы? Ответ: " + (bean3==bean4));
+    }
+}
